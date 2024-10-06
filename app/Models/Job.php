5ca4664d6253f1989e75e272f08b561f5159892d
Kesh <59233228+Kezia-Cane@users.noreply.github.com>
2024-10-07 05:15:36 +0800

    introduction to eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,24 +1,10 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-  public static function all(): array {
-    return [
-      ['id' => 1, 'title' => 'Software Developer', 'description' => 'Develop high-quality software applications.'],
-      ['id' => 2, 'title' => 'Frontend Developer', 'description' => 'Build responsive and user-friendly web applications.'],
-      ['id' => 3, 'title' => 'Backend Developer', 'description' => 'Design and implement backend systems for web applications.'],
-      ['id' => 4, 'title' => 'Full Stack Developer', 'description' => 'Develop web applications using both frontend and backend technologies.'],
-    ];
-  }
+class Job extends Model{
+  protected $table = 'job_listings';
 
-  public static function find(int $id) {
-    $job = \Illuminate\Support\Arr::first(Job::all(), fn(  $job) => $job['id'] == $id);
-
-    if (! $job) {
-      abort(404);
-    }
-
-    return $job;
-  }
+  protected $fillable = ['title', 'description'];
 }
